c04b08b651b660513b510e2de28782ed21c02a78
Amir Kozhanov <amirkozanov@gmail.com>
2023-04-04 21:02:22 +0600

    fix z-index & first section

--- src/js/main.js
+++ src/js/main.js
@@ -73,3 +73,15 @@ window.addEventListener('scroll', () => {
         header.classList.remove('fixed');
     }
 });
+
+
+// SWIPER
+
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
